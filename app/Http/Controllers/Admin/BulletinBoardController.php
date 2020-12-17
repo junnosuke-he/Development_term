402bf141c6b71db25b9f8b4143e7f88d10d9b14c
”junnosuke-he” <goukagu@gmail.com>
2020-12-17 01:58:23 +0000

    更新

--- app/Http/Controllers/Admin/BulletinBoardController.php
+++ app/Http/Controllers/Admin/BulletinBoardController.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers\Admin;
 
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
-
+use App\Bulletin_Board;
 class BulletinBoardController extends Controller
 {
     //

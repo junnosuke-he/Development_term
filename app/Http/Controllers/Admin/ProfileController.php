14a0c6218f15b356f00d089b54265d0d49b38787
”junnosuke-he” <goukagu@gmail.com>
2020-12-17 05:30:39 +0000

    更新

--- app/Http/Controllers/Admin/ProfileController.php
+++ app/Http/Controllers/Admin/ProfileController.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers\Admin;
 
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
-
+use App\Profile;
 class ProfileController extends Controller
 {
     //
@@ -14,6 +14,19 @@ class ProfileController extends Controller
     }
     public function create(Request $request)
     {
-      return redirect('admin/news/create');
+        $this->validate($request, Profile::$rules);
+        $profile = new Profile;
+        $form = $request->all();
+        if (isset($form['image'])) {
+              $path = $request->file('image')->store('public/image');
+              $profile->image_path = basename($path);
+          } else {
+              $profile->image_path = null;
+          }
+      unset($form['_token']);
+      unset($form['image']);
+      $profile->fill($form);
+      $profile->save();
+      return redirect('admin/profile/create');
     }  
 }
